ec1ee9144533bba3a4e8307a4cb8d8a3d9c0a1eb
Nicolás Giordano <34108039+NicolasEGiordano@users.noreply.github.com>
2021-11-03 20:12:07 -0300

    Add files via upload

---
+++ Zimmer/src/test/java/com/zimmer/proyect/ZimmerApplication.java
@@ -0,0 +1,13 @@
+package com.zimmer.proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
