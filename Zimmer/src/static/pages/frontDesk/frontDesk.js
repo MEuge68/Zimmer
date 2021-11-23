4770fd2246611f24fbcd758a4e87ec5a017f17d9
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-23 20:41:01 -0300

    added cards rendering with states

--- Zimmer/src/static/pages/frontDesk/frontDesk.js
+++ Zimmer/src/static/pages/frontDesk/frontDesk.js
@@ -4,11 +4,15 @@ const header = document.querySelector(".header");
 const spanToggle = document.querySelector(".header .toggle-btn span");
 let cardContainer = document.querySelector(".section_rooms_cardContainer");
 
+
 let habitaciones = "";
 let data = "";
 let cardData = "";
 
 console.log(cardContainer);
+
+
+
 
 
 
@@ -25,31 +29,89 @@ setInterval(async () => {
         method: "GET"
     });
     let data = await habitaciones.json(habitaciones)
+    const arrayEstados = ["disponible","sucia_desocupada","sucia_ocupada","limpia_ocupada","mantenimiento_ocupada","mantenimiento_desocupada"];
+    let arrayEstadoSeleccionado = "disponible";
+
+    if(arrayEstadoSeleccionado.includes("sucia")){
+        claseCss = "habitacion__sucia";
+    }else if(arrayEstadoSeleccionado.includes("limpia") || arrayEstadoSeleccionado.includes("disponible")){
+        claseCss = "habitacion__limpia";
+    }else if(arrayEstadoSeleccionado.includes("mantenimiento")){
+        claseCss = "habitacion__mantenimiento";
+    }
 
     for(let i=0;i<data.length;i++){
         cardData += `
-            <div class="section_rooms_cardContainer_card">
+            <div class="section_rooms_cardContainer_card ${claseCss}">
                 <h3 class="section_rooms_cardContainer_card_id"> ${data[i].id} </h3>
                 <h4 class="section_rooms_cardContainer_card_name"> ${data[i].name} </h4>
                 <div class="section_rooms_cardContainer_card_states">
-                    <select>
-                        <option value="sucia_desocupada">Sucia-Desocupada</option>
-                        <option value="sucia_ocupada">Sucia-Ocupada</option>
-                        <option value="disponible">Disponible</option>
-                        <option value="limpia_ocupada">Limpia-Ocupada</option>
-                        <option value="mantenimiento_ocupada">Mantenimiento-Ocupada</option>
-                        <option value="mantenimiento_desocupada">Mantenimiento-Desocupada</option>
+                
+        `
+
+        let estadosCarta = arrayEstados.filter((estado) => estado !== arrayEstadoSeleccionado)
+
+        cardData += `
+                    <select class="section_rooms_cardContainer_card_states_select ">
+                        <option value=${arrayEstadoSeleccionado}>${arrayEstadoSeleccionado}</option>
+                        <option value=${estadosCarta[0]}>${estadosCarta[0]}</option>
+                        <option value=${estadosCarta[1]}>${estadosCarta[1]}</option>
+                        <option value=${estadosCarta[2]}>${estadosCarta[2]}</option>
+                        <option value=${estadosCarta[3]}>${estadosCarta[3]}</option>
+                        <option value=${estadosCarta[4]}>${estadosCarta[4]}</option>
                     </select>
                 </div>
             </div>
         `
+
     }
 
 
 
     cardContainer.innerHTML = `${cardData}`
 
+
     cardData = "";
+
+    let select = await document.querySelectorAll(".section_rooms_cardContainer_card_states_select")
+
+
+
+    for(let i=0;i<select.length;i++){
+        select[i].addEventListener("change",(event) =>{
+
+            let claseCss;
+            console.log(event.target.value);
+
+
+            if(event.target.value.includes("sucia")){
+                claseCss = "habitacion__sucia";
+            }else if(event.target.value.includes("limpia") || event.target.value.includes("disponible")){
+                claseCss = "habitacion__limpia";
+            }else if(event.target.value.includes("mantenimiento")){
+                claseCss = "habitacion__mantenimiento";
+            }
+
+
+            console.log(event.target.value)
+
+            console.log(event.target.parentElement);
+            console.log(event.target.parentNode);
+
+
+            console.log(claseCss);
+
+            // Eliminando clases de estilo anteriores
+
+            event.target.parentElement.parentElement.classList.remove("habitacion__sucia")
+            event.target.parentElement.parentElement.classList.remove("habitacion__limpia")
+            event.target.parentElement.parentElement.classList.remove("habitacion__mantenimiento")
+
+            // Añadiendo nueva clase de estilo
+
+            event.target.parentElement.parentElement.classList.add(claseCss)
+        })
+    }
 
 },(5000))
 
